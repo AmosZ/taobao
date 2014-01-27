ad81804b0a39139a000f14433e0bb1f94346ae89
AmosZ <zzqqking@gmail.com>
2014-01-27 13:45:40 +0800

    init

---
+++ taobao/settings.py
@@ -0,0 +1,16 @@
+# Scrapy settings for taobao project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'taobao'
+
+SPIDER_MODULES = ['taobao.spiders']
+NEWSPIDER_MODULE = 'taobao.spiders'
+
+ITEM_PIPELINES = ['taobao.pipelines.IPhone5SPipeline']
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'taobao (+http://www.yourdomain.com)'
---
+++ taobao/spiders/TaobaoSpider.py
@@ -0,0 +1,45 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from taobao.items import Category,Retailer
+from scrapy.http import Request
+from scrapy.shell import inspect_response
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import MapCompose, Join
+from scrapy import log
+
+class TaobaoSpider(Spider):
+    name = "taobao"
+    allowed_domains = ["taobao.com"]
+    start_urls = ["http://www.taobao.com/market/3c/phone_index.php"]
+
+    category_url_xpath = '//*[@id="guid-1355118887616"]/div/div/div/p[1]/a[1]/@href'
+    item_fields = {
+            'title': './/a/text()',
+            'url': './/a/@href'
+            }
+
+    def parse(self,response):
+        selector = Selector(response)
+        for url in selector.xpath(self.category_url_xpath).extract():
+            yield Request(url,callback=self.parse_list)
+
+    def parse_list(self,response):
+#        inspect_response(response, self)
+        retailer_list_xpath = '//h3[@class="title"]'
+        sel = Selector(response)
+        for s in sel.xpath(retailer_list_xpath):
+            loader = ItemLoader(Retailer(),selector=s)
+
+            # define processors
+            loader.default_input_processor = MapCompose(unicode.strip)
+#            loader.default_output_processor = Join()
+
+            # iterate over fields and add xpaths to the loader
+            for field, xpath in self.item_fields.iteritems():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
+
+#        for s in sel.xpath(retailer_list_xpath):
+#            log.msg(s.xpath('a/text()').extract(),log.DEBUG)
+#            print(s.xpath('a/text()').extract())
+#            loader = ItemLoader(Retailer(),selector=s)
